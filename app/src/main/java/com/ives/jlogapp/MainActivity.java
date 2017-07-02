01bc2d545023638928a9c3e7effe5406489a5951
IvesWang <youji3hao@gmail.com>
2017-07-02 18:14:50 +0800

    invoke JLog

--- app/src/main/java/com/ives/jlogapp/MainActivity.java
+++ app/src/main/java/com/ives/jlogapp/MainActivity.java
@@ -3,11 +3,21 @@ package com.ives.jlogapp;
 import android.app.Activity;
 import android.os.Bundle;
 
+import com.ives.jlog.JLog;
+
 public class MainActivity extends Activity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        someLogs();
+    }
+    private void someLogs(){
+        String aa = "a string here";
+
+
+        JLog.d(2, "sf", aa);
     }
 }
